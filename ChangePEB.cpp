0f479ecdfade62b37008c9ed51c9bb8c2e4b477a
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-03-18 03:56:48 -0400

    Fixed spacing, cleaned up defines

--- ChangePEB.cpp
+++ ChangePEB.cpp
@@ -3,8 +3,10 @@
 
 #ifdef _WIN64
 #define IS_64_BIT 1
+#define SIZE_PEB 0x2000
 #else
 #define IS_64_BIT 0
+#define SIZE_PEB 0x1000 
 #endif
 
 #include <windows.h>
@@ -12,59 +14,52 @@
 #include <winternl.h>
 
 #if IS_64_BIT
-#define SIZE_PEB 0x2000 
-#else
-#define SIZE_PEB 0x1000 
-#endif
-
-#if IS_64_BIT
-
-typedef struct _TEB
-{
-	PVOID Reserved1[12];
-	PVOID ProcessEnvironmentBlock;
-} TEB, *PTEB;
-
 UINT64 GetPEBPointerAddress()
 {
-	PTEB teb = (PTEB)__readgsqword(0x30);
-	
-	if (teb == NULL)
-		return NULL;
+    typedef struct _TEB
+    {
+        PVOID Reserved1[12];
+        PVOID ProcessEnvironmentBlock;
+    } TEB, *PTEB;
 
-	return (UINT64)&(teb->ProcessEnvironmentBlock);
+    PTEB teb = (PTEB)__readgsqword(0x30);
+
+    if (teb == NULL)
+        return NULL;
+
+    return (UINT64)&(teb->ProcessEnvironmentBlock);
 }
 
-PVOID GetPEBAddress() 
+PVOID GetPEBAddress()
 {
-	PTEB teb = (PTEB)__readgsqword(0x30);
-	if (teb == NULL) 
-		return NULL;
-	
-	return teb->ProcessEnvironmentBlock;
+    PTEB teb = (PTEB)__readgsqword(0x30);
+    if (teb == NULL)
+        return NULL;
+
+    return teb->ProcessEnvironmentBlock;
 }
 
 void SetPEBAddress(UINT64 address)
 {
-	__try
-	{
-		UINT64 PEBPtrInTEB = GetPEBPointerAddress();
-		*(UINT64*)PEBPtrInTEB = address;
-	}
-	__except (EXCEPTION_EXECUTE_HANDLER)
-	{
-		printf("Failed at SetPEBAddress: memory exception writing PEB ptr\n");
-		return;
-	}
+    __try
+    {
+        UINT64 PEBPtrInTEB = GetPEBPointerAddress();
+        *(UINT64*)PEBPtrInTEB = address;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        printf("Failed at SetPEBAddress: memory exception writing PEB ptr\n");
+        return;
+    }
 }
 #else
 PVOID GetPEBAddress()
 {
-	PVOID pebAddress = 0;
+    PVOID pebAddress = 0;
 
     __asm
     {
-        mov eax, fs:[0x30]  //Offset of PEB in TEB, change in x64
+        mov eax, fs:[0x30]
         mov pebAddress, eax
     }
 
@@ -81,54 +76,54 @@ void SetPEBAddress(DWORD address)
 }
 #endif
 
-BYTE* CopyPEBBytes(unsigned int pebSize) 
+BYTE* CopyPEBBytes(unsigned int pebSize)
 {
-	LPVOID pebAddress = GetPEBAddress();
+    LPVOID pebAddress = GetPEBAddress();
 
-	BYTE* peb_bytes = new BYTE[pebSize];
+    BYTE* peb_bytes = new BYTE[pebSize];
 
-	BOOL success = ReadProcessMemory(GetCurrentProcess(), pebAddress, peb_bytes, pebSize, NULL);
-	if (!success) 
-	{
-		printf("Failed to copy PEB bytes. Error: %d\n", GetLastError());
-		delete[] peb_bytes;
-		return NULL;
-	}
+    BOOL success = ReadProcessMemory(GetCurrentProcess(), pebAddress, peb_bytes, pebSize, NULL);
+    if (!success)
+    {
+        printf("Failed to copy PEB bytes. Error: %d\n", GetLastError());
+        delete[] peb_bytes;
+        return NULL;
+    }
 
-	return peb_bytes;
+    return peb_bytes;
 }
 
 BYTE* SetNewPEB() //in this example, we are copying the original PEB to a byte array and then setting the pointer to the PEB to our byte array.
 {
-	BYTE* newPeb = CopyPEBBytes(SIZE_PEB);
+    BYTE* newPeb = CopyPEBBytes(SIZE_PEB);
 
-	if (newPeb != NULL)
-	{
-		printf("Setting new PEB at: %llx\n", (UINT64)newPeb);
-		SetPEBAddress((UINT64)newPeb); //set address of PEB to our buffer
-	}
+    if (newPeb != NULL)
+    {
+        printf("Setting new PEB at: %llx\n", (UINT64)newPeb);
+        SetPEBAddress((UINT64)newPeb);
+    }
 
-	return newPeb;
+    return newPeb;
 }
 
-int main() 
+int main()
 {
-	UINT64 pebAddr_Original = (UINT64)GetPEBAddress();
-	printf("Original PEB: %llx\n", pebAddr_Original);
+    UINT64 pebAddr_Original = (UINT64)GetPEBAddress();
+    printf("Original PEB: %llx\n", pebAddr_Original);
 
-	BYTE* newPEBBytes = SetNewPEB(); //copy original PEB to byte*, change pointer of PEB to the byte*
-	
-	UINT64 newPebAddr = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
-	printf("New PEB address: %llx\n", newPebAddr);
+    BYTE* newPEBBytes = SetNewPEB(); //copy original PEB to byte*, change pointer of PEB to the byte*
 
-	SetPEBAddress(pebAddr_Original); //set the PEB pointer back to our original
+    UINT64 newPebAddr = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
+    printf("New PEB address: %llx\n", newPebAddr);
+
+    SetPEBAddress(pebAddr_Original); //set the PEB pointer back to our original
+
+    if ((UINT64)GetPEBAddress() == pebAddr_Original)
+        delete[] newPEBBytes; //now we can delete our new PEB byte array before program shutdown, otherwise delete[] will throw an exception since the byte array is being used as the PEB
+
+    pebAddr_Original = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
+    printf("PEB (reset to original): %llx\n", pebAddr_Original);
 	
-	if ((UINT64)GetPEBAddress() == pebAddr_Original)
-		delete[] newPEBBytes; //now we can delete our new PEB byte array before program shutdown, otherwise delete[] will throw an exception since the byte array is being used as the PEB
-	
-	pebAddr_Original = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
-	printf("PEB (reset to original): %llx\n", pebAddr_Original);
-	
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
