1abeea38fd39ad191f48451106a19b7511fcddd7
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-03-25 02:32:43 -0400

    Fixed x86 offsets in __asm blocks (was grabbing TEB before)

--- ChangePEB.cpp
+++ ChangePEB.cpp
@@ -13,117 +13,180 @@
 #include <stdio.h>
 #include <winternl.h>
 
+typedef struct _MYPEB {
+	UCHAR InheritedAddressSpace;
+	UCHAR ReadImageFileExecOptions;
+	UCHAR BeingDebugged;
+	UCHAR Spare;
+	PVOID Mutant;
+	PVOID ImageBaseAddress;
+	PEB_LDR_DATA* Ldr;
+	PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+	PVOID SubSystemData;
+	PVOID ProcessHeap;
+	PVOID FastPebLock;
+	PVOID FastPebLockRoutine;
+	PVOID FastPebUnlockRoutine;
+	ULONG EnvironmentUpdateCount;
+	PVOID* KernelCallbackTable;
+	PVOID EventLogSection;
+	PVOID EventLog;
+	PVOID FreeList;
+	ULONG TlsExpansionCounter;
+	PVOID TlsBitmap;
+	ULONG TlsBitmapBits[0x2];
+	PVOID ReadOnlySharedMemoryBase;
+	PVOID ReadOnlySharedMemoryHeap;
+	PVOID* ReadOnlyStaticServerData;
+	PVOID AnsiCodePageData;
+	PVOID OemCodePageData;
+	PVOID UnicodeCaseTableData;
+	ULONG NumberOfProcessors;
+	ULONG NtGlobalFlag;
+	UCHAR Spare2[0x4];
+	ULARGE_INTEGER CriticalSectionTimeout;
+	ULONG HeapSegmentReserve;
+	ULONG HeapSegmentCommit;
+	ULONG HeapDeCommitTotalFreeThreshold;
+	ULONG HeapDeCommitFreeBlockThreshold;
+	ULONG NumberOfHeaps;
+	ULONG MaximumNumberOfHeaps;
+	PVOID** ProcessHeaps;
+	PVOID GdiSharedHandleTable;
+	PVOID ProcessStarterHelper; //PPS_POST_PREOCESS_INIT_ROUTINE?
+	PVOID GdiDCAttributeList;
+	PVOID LoaderLock;
+	ULONG OSMajorVersion;
+	ULONG OSMinorVersion;
+	ULONG OSBuildNumber;
+	ULONG OSPlatformId;
+	ULONG ImageSubSystem;
+	ULONG ImageSubSystemMajorVersion;
+	ULONG ImageSubSystemMinorVersion;
+	ULONG GdiHandleBuffer[0x22];
+	PVOID ProcessWindowStation;
+} MYPEB, * PMYPEB;
+
 #if IS_64_BIT
 UINT64 GetPEBPointerAddress()
 {
-    typedef struct _TEB
-    {
-        PVOID Reserved1[12];
-        PVOID ProcessEnvironmentBlock;
-    } TEB, *PTEB;
+	typedef struct _TEB
+	{
+		PVOID Reserved1[12];
+		PVOID ProcessEnvironmentBlock;
+	} TEB, *PTEB;
 
-    PTEB teb = (PTEB)__readgsqword(0x30);
+	PTEB teb = (PTEB)__readgsqword(0x30);
 
-    if (teb == NULL)
-        return NULL;
+	if (teb == NULL)
+		return NULL;
 
-    return (UINT64)&(teb->ProcessEnvironmentBlock);
+	return (UINT64)&(teb->ProcessEnvironmentBlock);
 }
 
 PVOID GetPEBAddress()
 {
-    PTEB teb = (PTEB)__readgsqword(0x30);
-    if (teb == NULL)
-        return NULL;
+	PTEB teb = (PTEB)__readgsqword(0x30);
+	if (teb == NULL)
+		return NULL;
 
-    return teb->ProcessEnvironmentBlock;
+	return teb->ProcessEnvironmentBlock;
 }
 
 void SetPEBAddress(UINT64 address)
 {
-    __try
-    {
-        UINT64 PEBPtrInTEB = GetPEBPointerAddress();
-        *(UINT64*)PEBPtrInTEB = address;
-    }
-    __except (EXCEPTION_EXECUTE_HANDLER)
-    {
-        printf("Failed at SetPEBAddress: memory exception writing PEB ptr\n");
-        return;
-    }
+	__try
+	{
+		UINT64 PEBPtrInTEB = GetPEBPointerAddress();
+		*(UINT64*)PEBPtrInTEB = address;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+		printf("Failed at SetPEBAddress: memory exception writing PEB ptr\n");
+		return;
+	}
 }
 #else
 PVOID GetPEBAddress()
 {
-    PVOID pebAddress = 0;
+	PVOID pebAddress = 0;
 
-    __asm
-    {
-        mov eax, fs:[0x30]
-        mov pebAddress, eax
-    }
+	__asm
+	{
+		mov eax, fs:[0x18]
+		mov eax, [eax + 0x30]
+		mov pebAddress, eax
+	}
 
-    return pebAddress;
+	return pebAddress;
 }
 
 void SetPEBAddress(DWORD address)
 {
-    __asm
-    {
-        mov eax, address
-        mov fs : [0x30], eax
-    }
+	__asm
+	{
+		push ebx
+		mov eax,fs:[0x18]
+		mov eax, [eax + 0x30]
+		mov ebx, address
+		mov [eax], ebx
+		pop ebx
+	}
 }
 #endif
 
 BYTE* CopyPEBBytes(unsigned int pebSize)
 {
-    LPVOID pebAddress = GetPEBAddress();
+	LPVOID pebAddress = GetPEBAddress();
 
-    BYTE* peb_bytes = new BYTE[pebSize];
+	BYTE* peb_bytes = new BYTE[pebSize];
 
-    BOOL success = ReadProcessMemory(GetCurrentProcess(), pebAddress, peb_bytes, pebSize, NULL);
-    if (!success)
-    {
-        printf("Failed to copy PEB bytes. Error: %d\n", GetLastError());
-        delete[] peb_bytes;
-        return NULL;
-    }
+	BOOL success = ReadProcessMemory(GetCurrentProcess(), pebAddress, peb_bytes, pebSize, NULL);
+	if (!success)
+	{
+		printf("Failed to copy PEB bytes. Error: %d\n", GetLastError());
+		delete[] peb_bytes;
+		return NULL;
+	}
 
-    return peb_bytes;
+	return peb_bytes;
 }
 
 BYTE* SetNewPEB() //in this example, we are copying the original PEB to a byte array and then setting the pointer to the PEB to our byte array.
 {
-    BYTE* newPeb = CopyPEBBytes(SIZE_PEB);
+	BYTE* newPeb = CopyPEBBytes(SIZE_PEB);
 
-    if (newPeb != NULL)
-    {
-        printf("Setting new PEB at: %llx\n", (UINT64)newPeb);
-        SetPEBAddress((UINT64)newPeb);
-    }
+	if (newPeb != NULL)
+	{
+		printf("Setting new PEB at: %llx\n", (UINT64)newPeb);
+		SetPEBAddress((UINT64)newPeb);
+	}
 
-    return newPeb;
+	return newPeb;
 }
 
 int main()
 {
-    UINT64 pebAddr_Original = (UINT64)GetPEBAddress();
-    printf("Original PEB: %llx\n", pebAddr_Original);
+	UINT64 pebAddr_Original = (UINT64)GetPEBAddress();
+	printf("Original PEB: %llx\n", pebAddr_Original);
 
-    BYTE* newPEBBytes = SetNewPEB(); //copy original PEB to byte*, change pointer of PEB to the byte*
+	BYTE* newPEBBytes = SetNewPEB(); //copy original PEB to byte*, change pointer of PEB to the byte*
 
-    UINT64 newPebAddr = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
-    printf("New PEB address: %llx\n", newPebAddr);
+	_MYPEB* ourPEB = (_MYPEB*)&newPEBBytes[0];
 
-    SetPEBAddress(pebAddr_Original); //set the PEB pointer back to our original
+	printf("Being debugged: %d\n", ourPEB->BeingDebugged); //check if our PEB structure works from casted byte array
 
-    if ((UINT64)GetPEBAddress() == pebAddr_Original)
-        delete[] newPEBBytes; //now we can delete our new PEB byte array before program shutdown, otherwise delete[] will throw an exception since the byte array is being used as the PEB
+	UINT64 newPebAddr = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
+	printf("New PEB address: %llx\n", newPebAddr);
 
-    pebAddr_Original = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
-    printf("PEB (reset to original): %llx\n", pebAddr_Original);
-	
-    system("pause");
-    return 0;
+	SetPEBAddress(pebAddr_Original); //set the PEB pointer back to our original
+
+	if ((UINT64)GetPEBAddress() == pebAddr_Original)
+		delete[] newPEBBytes; //now we can delete our new PEB byte array before program shutdown, otherwise delete[] will throw an exception since the byte array is being used as the PEB
+
+	pebAddr_Original = (UINT64)GetPEBAddress(); //verify that original PEB pointer was written
+	printf("PEB (reset to original): %llx\n", pebAddr_Original);
+
+	system("pause");
+	return 0;
 }
